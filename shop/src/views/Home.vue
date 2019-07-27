3ce185408a115c8f278bf7b84c5905780125f428
11145 <1114513467@qq.com>
2019-07-27 11:23:16 +0800

    首页

--- shop/src/views/Home.vue
+++ shop/src/views/Home.vue
@@ -1,24 +1,45 @@
 <template>
-  <div class="home">
-   <div>
-     <van-nav-bar
-     title="首页">
+  <div class="containner">
+   <div class="nav">
+     <van-nav-bar title="首页" class="nav-title">
        <van-icon name="search" slot="left" />
        <van-icon name="shopping-cart-o" slot="right" />
      </van-nav-bar>
    </div>
    <!-- 轮播图 -->
    <div class="swppic">
-     <van-swipe :autoplay="3000">
-      <van-swipe-item v-for="(item,index) in imagesItem" :key="index" class="swppicItem">
+     <van-swipe :autoplay="3000" >
+      <van-swipe-item v-for="(item,index) in imagesItem" :key="index" class="swppicItem" >
         <img v-lazy="item.imgsrc" alt="">
         </van-swipe-item> 
     </van-swipe>
+   </div>
+   <!-- 热门商品 -->
+   <div class="hot">
+      <p class="hot-title">热门商品</p>
+       <swiper class="hot-swiper" :options="swiperOption">
+
+         <swiper-slide v-for="(item,index) in hotProducts" :key="index">
+           <div class="hot-swiper-content">
+             <img :src="item.img" alt="">
+             <div>{{item.name}}</div>
+             <div>￥{{item.price}}</div>
+           </div>
+         </swiper-slide>
+      
+      </swiper>
+   </div>
+   <!-- 推荐商品 -->
+   <div>
+     
    </div>
   </div>
 </template>
 
 <script>
+import 'swiper/dist/css/swiper.css'
+ 
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
 export default {
   data(){
     return{
@@ -33,19 +54,115 @@ export default {
           name:"img3",
           imgsrc:'https://img.yzcdn.cn/vant/apple-1.jpg'
         }
-      ]
-       
+      ],
+       hotProducts:[
+
+         {name:"手扶牧草收割机",
+          img:"http://www.nongji1688.com/file/upload/201905/07/0920193925858.jpg",
+         price:"3600",
+         company:"曲阜市重机械制造有限公司",
+         city:"曲阜市"
+         },{name:"手扶牧草收割机",
+          img:"http://www.nongji1688.com/file/upload/201905/07/0920193925858.jpg",
+         price:"3600",
+         company:"曲阜市重机械制造有限公司",
+         city:"曲阜市"
+         },{name:"手扶牧草收割机",
+          img:"http://www.nongji1688.com/file/upload/201905/07/0920193925858.jpg",
+         price:"3600",
+         company:"曲阜市重机械制造有限公司",
+         city:"曲阜市"
+         },{name:"手扶牧草收割机",
+          img:"http://www.nongji1688.com/file/upload/201905/07/0920193925858.jpg",
+         price:"3600",
+         company:"曲阜市重机械制造有限公司",
+         city:"曲阜市"
+         },{name:"手扶牧草收割机",
+          img:"http://www.nongji1688.com/file/upload/201905/07/0920193925858.jpg",
+         price:"3600",
+         company:"曲阜市重机械制造有限公司",
+         city:"曲阜市"
+         },{name:"手扶牧草收割机",
+          img:"http://www.nongji1688.com/file/upload/201905/07/0920193925858.jpg",
+         price:"3600",
+         company:"曲阜市重机械制造有限公司",
+         city:"曲阜市"
+         },{name:"手扶牧草收割机",
+          img:"http://www.nongji1688.com/file/upload/201905/07/0920193925858.jpg",
+         price:"3600",
+         company:"曲阜市重机械制造有限公司",
+         city:"曲阜市"
+         },{name:"手扶牧草收割机",
+          img:"http://www.nongji1688.com/file/upload/201905/07/0920193925858.jpg",
+         price:"3600",
+         company:"曲阜市重机械制造有限公司",
+         city:"曲阜市"
+         },{name:"手扶牧草收割机",
+          img:"http://www.nongji1688.com/file/upload/201905/07/0920193925858.jpg",
+         price:"3600",
+         company:"曲阜市重机械制造有限公司",
+         city:"曲阜市"
+         },{name:"手扶牧草收割机",
+          img:"http://www.nongji1688.com/file/upload/201905/07/0920193925858.jpg",
+         price:"3600",
+         company:"曲阜市重机械制造有限公司",
+         city:"曲阜市"
+         },
+      ],
+      swiperOption: {
+         slidesPerView:3
+        }
       
     }
+  },
+  components: {
+    swiper,
+    swiperSlide
   }
 }
 </script>
 <style lang="scss">
+.containner{
+  background: #ccc;
+  
+}
+
+
+.nav-title{
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 99!important;
+
+}
   .swppic{
     height:4rem;
+    margin-top: 1.5rem;
   }
   .swppicItem img{
     width: 100rem;
     height:4rem;
+  }
+  .hot{
+    margin-top: 0.2rem;
+    background: white;
+    &-title{
+      width: 100%;
+      height: 0.5rem;
+      padding-left: 0.2rem;
+      line-height: 0.5rem;
+      box-sizing: border-box;
+    }
+    &-swiper{
+      &-content{
+        width: 2rem;
+        text-align: center;
+        img{
+          width: 3rem;
+          height: 3rem;
+        }
+      }
+    }
   }
 </style>
